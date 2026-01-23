038f761904cc085b383f3adc861713dbc37d1e67
Christiane <Christiane>
2026-01-23 21:54:41 +0100

    Add PraxisId

--- Services/Authentication/AuthService.swift
+++ Services/Authentication/AuthService.swift
@@ -145,7 +145,8 @@ class AuthService: ObservableObject {
               password: password,
               firstName: firstName,
               lastName: lastName,
-              role: isTherapist ? .therapist : .patient
+              role: isTherapist ? .therapist : .patient,
+              praxisId: praxisId 
           )
           
           // ✅ Beide speichern
