a99b58eb0f274056758ccccdd8511b6353d1a27d
C8888-cr <christiane100385@web.de>
2026-02-23 11:17:02 +0100

    * Changed Therapists in Langen and Ostend in real Therapists

--- Manager/TherapeutDataManager.swift
+++ Manager/TherapeutDataManager.swift
@@ -27,21 +27,26 @@ class TherapeutDataManager {
     static let t7Id  = UUID(uuidString: "cccccccc-0001-0001-0001-cccccccccccc")!
     static let t8Id  = UUID(uuidString: "cccccccc-0002-0002-0002-cccccccccccc")!
     static let t9Id  = UUID(uuidString: "cccccccc-0003-0003-0003-cccccccccccc")!
+    static let t10Id = UUID(uuidString: "cccccccc-0004-0004-0004-cccccccccccc")!
+    static let t11Id = UUID(uuidString: "cccccccc-0005-0005-0005-cccccccccccc")!
+    
     
     // agil Ostend
-    static let t10Id = UUID(uuidString: "dddddddd-0001-0001-0001-dddddddddddd")!
-    static let t11Id = UUID(uuidString: "dddddddd-0002-0002-0002-dddddddddddd")!
-    static let t12Id = UUID(uuidString: "dddddddd-0003-0003-0003-dddddddddddd")!
+    static let t12Id = UUID(uuidString: "dddddddd-0001-0001-0001-dddddddddddd")!
+    static let t13Id = UUID(uuidString: "dddddddd-0002-0002-0002-dddddddddddd")!
+    static let t14Id = UUID(uuidString: "dddddddd-0003-0003-0003-dddddddddddd")!
+    static let t15Id = UUID(uuidString: "dddddddd-0004-0004-0004-dddddddddddd")!
+    static let t16Id = UUID(uuidString: "dddddddd-0005-0005-0005-dddddddddddd")!
     
     // agil Dornbusch
-    static let t13Id = UUID(uuidString: "eeeeeeee-0001-0001-0001-eeeeeeeeeeee")!
-    static let t14Id = UUID(uuidString: "eeeeeeee-0002-0002-0002-eeeeeeeeeeee")!
-    static let t15Id = UUID(uuidString: "eeeeeeee-0003-0003-0003-eeeeeeeeeeee")!
+    static let t17Id = UUID(uuidString: "eeeeeeee-0001-0001-0001-eeeeeeeeeeee")!
+    static let t18Id = UUID(uuidString: "eeeeeeee-0002-0002-0002-eeeeeeeeeeee")!
+    static let t19Id = UUID(uuidString: "eeeeeeee-0003-0003-0003-eeeeeeeeeeee")!
     
     // agil Alt-Eschersheim
-    static let t16Id = UUID(uuidString: "ffffffff-0001-0001-0001-ffffffffffff")!
-    static let t17Id = UUID(uuidString: "ffffffff-0002-0002-0002-ffffffffffff")!
-    static let t18Id = UUID(uuidString: "ffffffff-0003-0003-0003-ffffffffffff")!
+    static let t20Id = UUID(uuidString: "ffffffff-0001-0001-0001-ffffffffffff")!
+    static let t21Id = UUID(uuidString: "ffffffff-0002-0002-0002-ffffffffffff")!
+    static let t22Id = UUID(uuidString: "ffffffff-0003-0003-0003-ffffffffffff")!
     
     // MARK: - Alle Therapeuten
     let therapeuten: [Therapeut] = [
@@ -89,58 +94,82 @@ class TherapeutDataManager {
         // MARK: agil Langen
         Therapeut(
             id: TherapeutDataManager.t7Id,
-            firstName: "Nina",
-            lastName: "Hoffmann",
+            firstName: "Christiane",
+            lastName: "Roth",
             praxisId: PraxisDataManager.praxis3Id
         ),
         Therapeut(
             id: TherapeutDataManager.t8Id,
-            firstName: "Tim",
-            lastName: "Fischer",
+            firstName: "Kirstin",
+            lastName: "Colloseus",
             praxisId: PraxisDataManager.praxis3Id
         ),
         Therapeut(
             id: TherapeutDataManager.t9Id,
-            firstName: "Clara",
-            lastName: "Becker",
+            firstName: "Matthias",
+            lastName: "Thomczak",
+            praxisId: PraxisDataManager.praxis3Id
+        ),
+        Therapeut(
+            id: TherapeutDataManager.t10Id,
+            firstName: "Nella",
+            lastName: "Elkaz",
+            praxisId: PraxisDataManager.praxis3Id
+        ),
+        Therapeut(
+            id: TherapeutDataManager.t11Id,
+            firstName: "Sandra",
+            lastName: "Korn",
             praxisId: PraxisDataManager.praxis3Id
         ),
         
         // MARK: agil Ostend
         Therapeut(
-            id: TherapeutDataManager.t10Id,
-            firstName: "David",
-            lastName: "Schulz",
-            praxisId: PraxisDataManager.praxis4Id
-        ),
-        Therapeut(
-            id: TherapeutDataManager.t11Id,
-            firstName: "Marie",
-            lastName: "Richter",
-            praxisId: PraxisDataManager.praxis4Id
-        ),
-        Therapeut(
             id: TherapeutDataManager.t12Id,
-            firstName: "Paul",
-            lastName: "Wolf",
+            firstName: "Kerstin",
+            lastName: "Becker",
+            praxisId: PraxisDataManager.praxis4Id
+        ),
+        Therapeut(
+            id: TherapeutDataManager.t13Id,
+            firstName: "Luca",
+            lastName: "Leitsch",
+            praxisId: PraxisDataManager.praxis4Id
+        ),
+        Therapeut(
+            id: TherapeutDataManager.t14Id,
+            firstName: "Caroline",
+            lastName: "Lang",
+            praxisId: PraxisDataManager.praxis4Id
+        ),
+        Therapeut(
+            id: TherapeutDataManager.t15Id,
+            firstName: "Christiane",
+            lastName: "Roth",
+            praxisId: PraxisDataManager.praxis4Id
+        ),
+        Therapeut(
+            id: TherapeutDataManager.t16Id,
+            firstName: "Ruben",
+            lastName: "Hoffmann",
             praxisId: PraxisDataManager.praxis4Id
         ),
         
         // MARK: agil Dornbusch
         Therapeut(
-            id: TherapeutDataManager.t13Id,
+            id: TherapeutDataManager.t17Id,
             firstName: "Lisa",
             lastName: "Neumann",
             praxisId: PraxisDataManager.praxis5Id
         ),
         Therapeut(
-            id: TherapeutDataManager.t14Id,
+            id: TherapeutDataManager.t18Id,
             firstName: "Ben",
             lastName: "Zimmermann",
             praxisId: PraxisDataManager.praxis5Id
         ),
         Therapeut(
-            id: TherapeutDataManager.t15Id,
+            id: TherapeutDataManager.t19Id,
             firstName: "Jana",
             lastName: "Krause",
             praxisId: PraxisDataManager.praxis5Id
@@ -148,19 +177,19 @@ class TherapeutDataManager {
         
         // MARK: agil Alt-Eschersheim
         Therapeut(
-            id: TherapeutDataManager.t16Id,
+            id: TherapeutDataManager.t20Id,
             firstName: "Max",
             lastName: "Hartmann",
             praxisId: PraxisDataManager.praxis6Id
         ),
         Therapeut(
-            id: TherapeutDataManager.t17Id,
+            id: TherapeutDataManager.t21Id,
             firstName: "Eva",
             lastName: "Lange",
             praxisId: PraxisDataManager.praxis6Id
         ),
         Therapeut(
-            id: TherapeutDataManager.t18Id,
+            id: TherapeutDataManager.t22Id,
             firstName: "Lukas",
             lastName: "Bauer",
             praxisId: PraxisDataManager.praxis6Id
