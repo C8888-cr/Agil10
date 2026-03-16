f9b2a92d94ef228086642a56d49a3e778aac5ae2
C8888-cr <christiane100385@web.de>
2026-03-16 19:24:18 +0100

    * add Enum SheduleScope to plan videos in future or not

---
+++ Enum/ScheduleScope.swift
@@ -0,0 +1,14 @@
+//
+//  ScheduleScope.swift
+//  Agil10.0
+//
+//  Created by Christiane Roth on 14.03.26.
+//
+
+
+import Foundation
+
+enum ScheduleScope {
+    case onlyToday
+    case allFuture
+}
