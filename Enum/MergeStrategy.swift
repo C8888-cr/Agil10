147d4afb99ba6476267abb868338c4e365ee4aeb
C8888-cr <christiane100385@web.de>
2026-03-16 19:26:32 +0100

    * enum MergeStrategy for videos. replace or keep videos

---
+++ Enum/MergeStrategy.swift
@@ -0,0 +1,12 @@
+//
+//  MergeStrategy.swift
+//  Agil10.0
+//
+//  Created by Christiane Roth on 15.03.26.
+//
+import Foundation
+
+enum MergeStrategy {
+    case replaceAll      // Alles ersetzen
+    case addToPlan       // Plan hinzufügen, manuelle behalten
+}
