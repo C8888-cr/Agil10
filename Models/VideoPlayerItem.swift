20a709048c6d5295ad617dcd4c346a34ec1d05f0
Christiane <Christiane>
2026-03-22 21:03:04 +0100

    add VideoPlayerItem to play videos direct after login

---
+++ Models/VideoPlayerItem.swift
@@ -0,0 +1,13 @@
+//
+//  VideoPlayerItem.swift
+//  Agil10.0
+//
+//  Created by Christiane Roth on 22.03.26.
+//
+import SwiftUI
+
+struct VideoPlayerItem: Identifiable {
+    let id = UUID()
+    let video: Video
+    let scheduleId: UUID
+}
